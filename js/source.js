45703f04d177d6284c2d3e8c30085754f8f5bf75
kanchana208 <43988861+kanchana208@users.noreply.github.com>
2018-10-09 15:59:07 +0530

    Changing to Joe

--- js/source.js
+++ js/source.js
@@ -1,7 +1,7 @@
 window.fcWidget.init({ token: "87cc47b8-bbdc-4cd5-aacd-e4f8a57765c5", host: "https://wchat.freshchat.com" });
 window.fcWidget.setExternalId("joe.smith");
  // To set user name
- window.fcWidget.user.setFirstName("Tim");
+ window.fcWidget.user.setFirstName("Joe");
  window.fcWidget.user.setLastName("Smith");
 
  // To set user email
