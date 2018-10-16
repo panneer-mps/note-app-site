b8c5365d6a1616fca34bcfab5557fdb4cd1bc572
kanchana208 <43988861+kanchana208@users.noreply.github.com>
2018-10-16 19:15:38 +0530

    Update source.js

--- js/source.js
+++ js/source.js
@@ -1,4 +1,4 @@
- window.fcWidget.init({ token: "87cc47b8-bbdc-4cd5-aacd-e4f8a57765c5", host: "https://wchat.freshchat.com" });
+// window.fcWidget.init({ token: "87cc47b8-bbdc-4cd5-aacd-e4f8a57765c5", host: "https://wchat.freshchat.com" });
 // window.fcWidget.setExternalId("joe.smith");
  // To set user name
  // window.fcWidget.user.setFirstName("Joe");
